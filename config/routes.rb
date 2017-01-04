9937f44a617d3ed7be50a2fd85c7018d56ef865d
brittany heaton <bheaton@arrayschool.com>
2017-01-04 11:29:36 -0700

    actually added stuff missed.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  resources :microposts
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/hello', to: 'application#hello'
   get '/goodbye', to: 'application#goodbye'
 
   #changing root (homepage) from rails splace to new page
   #get '/', to: 'applocation#hello'   or
-  root 'application#hello'
+  root 'users#index'
 end
